ba34fc33243cd3813cb56248d75d7ae3b77af38f
Xxmostafaahmed <92220702+Xxmostafaahmed@users.noreply.github.com>
2021-11-11 23:03:40 +0530

    Add files via upload

---
+++ Operators.py
@@ -0,0 +1,27 @@
+#arithmetic operators
+a=5
+b=8
+print(a+b)
+print(a-b)
+print(a*b)
+print(a//b)
+print(a**b)
+
+#assignment operators
+m= 4
+m= m+4
+
+print(m)
+#relation operators is used for comparison
+
+
+#logical operators
+
+x= True
+
+y=False
+print('x and y is ',x and y)
+print('x and y is ',x or y)
+print('x and y is ',not y)
+
+
