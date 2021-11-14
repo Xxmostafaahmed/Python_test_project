f9547463564f85ef63f40442038bf0ddb5d69091
Xxmostafaahmed <92220702+Xxmostafaahmed@users.noreply.github.com>
2021-11-14 21:45:07 +0530

    Add files via upload

---
+++ loops/Break,continue,pass.py
@@ -0,0 +1,22 @@
+#Break
+for i in range(10):
+
+
+    if i>=7:
+
+        break
+
+
+    print(i)
+    
+#Continue
+
+for i in range(10):
+
+
+    if i==6:
+        
+        continue
+
+
+    print(i)
---
+++ loops/For Loop.py
@@ -0,0 +1,3 @@
+for x in range(10):
+#the vlue of x [0 to 9]
+    print(2*x , end=" , ")
