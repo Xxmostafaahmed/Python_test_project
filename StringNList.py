fa834e5ba4b473ed8dc9ab2ab63e753a5cc51065
Xxmostafaahmed <92220702+Xxmostafaahmed@users.noreply.github.com>
2021-11-14 21:46:13 +0530

    Add files via upload

---
+++ StringNList.py
@@ -0,0 +1,31 @@
+fruit='Apple'
+print(fruit[2])
+
+#python string method 
+
+a='abcd'
+print(a.isalpha())
+
+
+#
+
+m='1234'
+print(m.isdigit())
+
+#list
+
+fruits = ["Apple","Guava","Papaya","Banana"]
+#by using this we can change "fruits[1]"
+#for deler we use del "del fruits[3]"
+fruits[1]="Mango"
+del fruits[3]
+print(fruits[0])
+print(fruits[1])
+print(fruits[2])
+print(fruits[-1])
+#print(fruits[5])
+
+
+
+
+
